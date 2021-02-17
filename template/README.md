0c9957a54fed03c29f0d5f71925611f15089693f
Michael <michael@michaeldev.com>
2021-02-17 16:51:00 +0000

    Initial template files

---
+++ template/README.md
@@ -0,0 +1,3 @@
+# React Snowman Game Tutorial
+
+This tutorial shows how to build a simple Hangman style game using React.
---
+++ template/src/Game.js
@@ -0,0 +1,15 @@
+import './Game.css';
+
+function Game() {
+  return (
+    <div className="game-wrapper">
+      <div className="message">
+        <h3>Snowman Tutorial</h3>
+        <p>Building a simple game using React</p>
+        <small>ig: @masmith_dev<br />www.masmithdev.com</small>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
